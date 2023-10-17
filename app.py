ac5c2957957458c7b370af743feb532f86156cf2
panrasch <panrasch@gmail.com>
2023-10-17 09:26:07 +0200

    uri

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+from neo4j import GraphDatabase, Driver, AsyncGraphDatabase, AsyncDriver
+import re
 from flask import Flask, render_template, redirect, request
 
+URI = "neo4j+s://"
+AUTH = ("neo4j", "")
+
 app = Flask(__name__)
+

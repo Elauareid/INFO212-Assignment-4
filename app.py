b8f7cb43c51c72cee16f8ff0388aad493b613af2
Erik Lauareid <erik@lauareid.no>
2023-10-17 09:30:23 +0200

    auth

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from neo4j import GraphDatabase, Driver, AsyncGraphDatabase, AsyncDriver
 import re
 from flask import Flask, render_template, redirect, request
 
-URI = "neo4j+s://"
-AUTH = ("neo4j", "")
+URI = "neo4j+s://1d9eb7a2.databases.neo4j.io:7687"
+AUTH = ("neo4j", "GB2CSsX2B-dgthvoGxcaStQD1t1AoO0vVClJeSuh0rI")
 
 app = Flask(__name__)
 
